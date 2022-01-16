c1847a6ef00580a9efa532ca97c14c91193da951
Anmol Singh <anmolbuttar143205@gmail.com>
2022-01-16 07:14:31 -0800

    added devlopement api  ednpoint

--- src/services/api/provider.js
+++ src/services/api/provider.js
@@ -6,11 +6,12 @@ import { handleResponse, handleError } from './response';
 // Define your api url from any source.
 // Pulling from your .env file when on the server or from localhost when locally
 const BASE_URL = 'http://127.0.0.1:3333/api/v1';
+const dev_URL = 'http://localhost:5000/';
 
 /* @param {string} resource */
 const getAll = (resource) => {
     return axios
-        .get(`${BASE_URL}/${resource}`)
+        .get(`${ dev_URL}/${resource}`)
         .then(handleResponse)
         .catch(handleError);
 };
@@ -19,7 +20,7 @@ const getAll = (resource) => {
 /* @param {string} id */
 const getSingle = (resource, id) => {
     return axios
-        .get(`${BASE_URL}/${resource}/${id}`)
+        .get(`${ dev_URL}/${resource}/${id}`)
         .then(handleResponse)
         .catch(handleError);
 };
@@ -28,7 +29,7 @@ const getSingle = (resource, id) => {
 /* @param {object} model */
 const post = (resource, model) => {
     return axios
-        .post(`${BASE_URL}/${resource}`, model)
+        .post(`${ dev_URL}/${resource}`, model)
         .then(handleResponse)
         .catch(handleError);
 };
@@ -37,7 +38,7 @@ const post = (resource, model) => {
 /* @param {object} model */
 const put = (resource, model) => {
     return axios
-        .put(`${BASE_URL}/${resource}`, model)
+        .put(`${ dev_URL}/${resource}`, model)
         .then(handleResponse)
         .catch(handleError);
 };
@@ -46,7 +47,7 @@ const put = (resource, model) => {
 /* @param {object} model */
 const patch = (resource, model) => {
     return axios
-        .patch(`${BASE_URL}/${resource}`, model)
+        .patch(`${ dev_URL}/${resource}`, model)
         .then(handleResponse)
         .catch(handleError);
 };
@@ -55,7 +56,7 @@ const patch = (resource, model) => {
 /* @param {string} id */
 const remove = (resource, id) => {
     return axios
-        .delete(`${BASE_URL}/${resource}`, id)
+        .delete(`${ dev_URL}/${resource}`, id)
         .then(handleResponse)
         .catch(handleError);
 };
