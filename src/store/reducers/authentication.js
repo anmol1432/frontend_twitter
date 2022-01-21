7e8e69a84c19cd96e04097190e5f5c2a49506393
Anmol Singh <anmolbuttar143205@gmail.com>
2022-01-20 21:58:53 -0800

    create authentication.js

--- src/store/reducers/authentication.js
+++ src/store/reducers/authentication.js
@@ -1,4 +1,5 @@
 import { signIn } from "../action/authentication";
+import { SIGN_IN } from "../../services/endpoints/authentication";
 
 export const initialState = {
     signup: {
@@ -12,6 +13,7 @@ export const initialState = {
 const authentication = (state = initialState, action) => {
     switch (action.type) {
         case signIn:
+            
             return {...state }
         default:
             return state
