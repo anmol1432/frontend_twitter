b48dd6d7b2dabfce5d449e7402fb63e810378005
Anmol Singh <anmolbuttar143205@gmail.com>
2022-02-19 10:43:12 -0600

    implementing profile section working viewproifle component

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
+import "./index.css";
 
 const ViewProfile = () => {
     return (
         <>
             <div className="holder">
+                <div className='' id='bg-banner'>
+                    <img className="w-full " src="https://pbs.twimg.com/profile_banners/1148089534967476225/1593610688/600x200" id='banner' alt="" />
+                </div>
 
-                <div className="card border w-96 hover:shadow-none relative flex flex-col mx-auto shadow-lg m-5">
-                    <img className="max-h-20 w-full opacity-80 absolute top-0" style={{ zIndex: -1 }} src="https://unsplash.com/photos/h0Vxgz5tyXA/download?force=true&w=640" alt="" />
-                    <div className="profile w-full flex m-3 ml-4 text-white">
-                        <img className="w-28 h-28 p-1  rounded-full" src="https://images.pexels.com/photos/61100/pexels-photo-61100.jpeg?crop=faces&fit=crop&h=200&w=200&auto=compress&cs=tinysrgb" alt="" />
-                    </div>
-                    <div className="buttons flex absolute bottom-0 font-bold right-0 text-xs text-gray-500 space-x-0 my-3.5 mr-3">
-                        <div className="add border rounded-l-2xl rounded-r-sm border-gray-300 p-1 px-4 cursor-pointer hover:bg-gray-700 hover:text-white">Contact</div>
-                        {/* <!-- <div className="add border rounded-r-2xl rounded-l-sm border-gray-300 p-1 px-4 cursor-pointer hover:bg-gray-700 hover:text-white">Bio</div> --> */}
+                <div id="profile-info">
+                    <div className='flex justify-between h-12'>
+                        <img className="w-32 h-32 rounded-full relative left-4 border-4 border-black " src="https://pbs.twimg.com/profile_images/1480771362855686145/WNjPqVGK_400x400.jpg" style={{ top: '-60px' }} alt="" />
+
+                        < button type="primary" shape="round" size="large" className="button bg-transparent border-2  rounded-full font-medium text-white px-4  mt-2 mr-4 items-end lg:visible md:invisible invisible" style={{ 'border': '2px solid #1f2937' }}>
+                            Edit profile
+                        </button>
                     </div>
                 </div>
             </div>
+
         </>
     )
 }
