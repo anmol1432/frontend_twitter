65d2764110f4a7d75f5e5f4d0ed82ab35f59c0e8
Anmol Singh <anmolbuttar143205@gmail.com>
2022-02-19 10:43:32 -0600

    fixing style in sidebar

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -11,8 +11,8 @@ import './index.css'
 const SingleLink = ({ linkText, icon, adress }) => {
     return (
         <>
-            <NavLink to={String(adress)} >
-                <div className="flex items-center p-2  rounded-full tracking-wide font-semibold focus:outline-none focus:shadow-outline hover:bg-gray-800 shadow-lg cursor-pointer transition ease-in duration-300">
+            <NavLink to={String(adress)}>
+                <div className="flex items-center p-2  rounded-full tracking-wide font-semibold focus:outline-none focus:shadow-outline hover:bg-gray-800 shadow-lg cursor-pointer transition ease-in duration-300" >
                     <div className="icons lg:w-3/12 w-full text-center rounded-full border-gray-100" style={{
                         height: "35px",
                         padding: "2px",
@@ -68,7 +68,7 @@ const SideBar = () => {
         {
             linkText: 'Profile',
             icon: <UserOutlined className="fa fa-home stroke-current text-gray-100  text-2xl " />,
-            adress: `/pofile/123`
+            adress: `/profile/123`
         },
         {
             linkText: 'More',
@@ -80,8 +80,8 @@ const SideBar = () => {
         <div>
             <div className=" " id="sidebar">
                 {/*  */}
-                <div className="flex items-center space-x-0 ">
-                    <svg viewBox="0 0 24 24" className="my-6  lg:h-8 h-12 lg:visible invisible  " fill="currentColor">
+                <div className="flex items-center justify-center  pl-4 ">
+                    <svg viewBox="0 0 24 24" className="my-6  lg:h-9 h-12 lg:visible invisible  " fill="currentColor">
                         <path className="text-gray-100" d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49
                         2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658
                         2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63
@@ -96,15 +96,19 @@ const SideBar = () => {
                 {/*  */}
                 <div className="block m-auto ">
                     <div className="flex flex-col  justify-around  items-start lg:rounded-xl h-4/6" id="sidebarList">
-                        {navTabs.map((item) => SingleLink({ ...item }))}
-                        <button type="primary" shape="round" size="large" className="button bg-blue-500 rounded-full font-medium text-white w-8/12 py-3 mt-2 items-end lg:visible md:invisible invisible" id="icons-hide">
+                        {navTabs.map((item) => {
+                            return (<>
+                                <SingleLink linkText={item.linkText} icon={item.icon} adress={item.icon} key={item.linkText} />
+                            </>)
+                        })}
+                        <button type="primary" shape="round" size="large" className="button bg-blue-500 rounded-full font-medium text-white  w-full py-3 mt-2 items-end lg:visible md:invisible invisible" id="icons-hide">
                             Tweet
                         </button>
                         {
                             localStorage.getItem("token") ? < button onClick={() => {
                                 localStorage.removeItem("token")
                                 history.push("/login")
-                            }} type="primary" shape="round" size="large" className="button bg-transparent border-2  rounded-full font-medium text-white w-8/12 py-3 mt-2 items-end lg:visible md:invisible invisible" id="icons-hide">
+                            }} type="primary" shape="round" size="large" className="button bg-transparent border-2  rounded-full font-medium text-white w-full py-3 mt-2 items-end lg:visible md:invisible invisible" id="icons-hide">
                                 logout
                             </button> : <></>
                         }
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ComposeTweet from "../../components/ComposeTweet/index";
-import MainLayout from "../../components/MainLayout/index";
-import ViewProfile from "../../components/Profile/index";
-import stars from "../../Assets/img/stars.png";
+import React from 'react';
+import ComposeTweet from '../../components/ComposeTweet/index';
+import MainLayout from '../../components/MainLayout/index';
+import ViewProfile from '../../components/Profile/index';
+import stars from '../../Assets/img/stars.png';
 import { ArrowLeftOutlined } from '@ant-design/icons';
-import "./index.css"
+import './index.css';
 
 const Profile = () => {
     return (
@@ -17,21 +17,21 @@ const Profile = () => {
                             <img
                                 className="rounded-full w-10 lg:invisible lg:w-0"
                                 src="https://images.unsplash.com/photo-1638913976381-5b8ed66c36d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-                                onClick={() => ""}
+                                onClick={() => ''}
                                 alt="profile_images"
                             />
                             <h1 className="lg:m-0 ml-6 flex ">
-                                <ArrowLeftOutlined className="p-2 mr-2 rounded-full hover:bg-slate-800" />
+                                <ArrowLeftOutlined color={'#3b82f6'} className="p-2 mr-2 rounded-full hover:bg-slate-800" />
                                 <span>⚔️ ANMOL SINGH ☠️</span>
                             </h1>
                         </span>
                     </div>
                     <div id="border">
-                        <ViewProfile />
+                        <ViewProfile id="border" />
                     </div>
                 </>} />
         </>
-    )
-}
+    );
+};
 
-export default Profile
+export default Profile;
